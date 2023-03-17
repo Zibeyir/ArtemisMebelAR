adfd0ed194168351e35b6724ee6f54913d6d13e5
Ziko Aghaeyev <zibeyira@gmail.com>
2023-03-17 12:07:18 +0400

    MebelPosText

--- Unity/Artemis/Assets/Scripts/Mebel.cs
+++ Unity/Artemis/Assets/Scripts/Mebel.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UniGLTF;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using static System.Net.WebRequestMethods;
 
@@ -14,13 +15,19 @@ public class Mebel : MonoBehaviour
     public Transform[] MebelParts;
 
     public GameObject MebelObject;
-
+    static bool DownlandMebelBool = false;
+    public static Transform _mebelPos;
+    [SerializeField] Transform CameraPos;
+    [SerializeField] Text MebelCameraPosText;
     void Start()
     {
         //MebelParts = MebelObject.GetComponentsInChildren<Transform>();
+        //StartCoroutine(downloadGLTF());
+    }
+    public void DownloadModel()
+    {
         StartCoroutine(downloadGLTF());
     }
-
     IEnumerator downloadGLTF()
     {
         //   string url = "https://firebasestorage.googleapis.com/v0/b/unity-test-4a52d.appspot.com/o/metbex_mebeli.gltf?alt=media&token=59d0e7b0-0c4d-43c3-8be3-51aafae704a5";
@@ -35,19 +42,31 @@ public class Mebel : MonoBehaviour
             Debug.Log(req.error);
             yield return null;
         }
+        MebelCameraPosText.text = "LoadStart";
+
         LoadModel(filePath);
+        MebelCameraPosText.text = "LoadFinish";
+
     }
-    public static void LoadModel(string path)
+    private void Update()
     {
-        print(path);
+        if (DownlandMebelBool)
+        {
+            MebelCameraPosText.text = "MebelPos " + _mebelPos.position + "Camera " + CameraPos.position; 
+        }
+    }
+    private static void LoadModel(string path)
+    {
         var context = gltfImporter.Load(path);
         context.ShowMeshes();
         GameObject root = context.Root;
-        root.transform.localScale = new Vector3(1f, 1f, 1f);
+        _mebelPos = root.transform;
+        root.transform.localScale = new Vector3(.1f, .1f, .1f);
         Vector3 origin = new Vector3(0f, 0f, 0f);
         Vector3 pos = origin;// + UnityEngine.Random.onUnitSphere * radius;
         root.transform.position = new Vector3(pos.x, 0, pos.z);
         // root.transform.LookAt(origin);
         // root.transform.SetParent(model.transform);
+        DownlandMebelBool = true;
     }
 }
